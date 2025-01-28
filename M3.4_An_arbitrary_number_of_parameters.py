71c79217a58d1e75e9134b89b370928f64028625
Аксан <a.davletkujin@gmail.com>
2025-01-28 23:04:30 +0500

    Create M3.4_An_arbitrary_number_of_parameters.py

---
+++ M3.4_An_arbitrary_number_of_parameters.py
@@ -0,0 +1,17 @@
+# Однокоренные
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word = root_word.upper()
+    other_words = list(map(lambda s: s.upper(), other_words))
+
+    for i in other_words:
+        if root_word in i or i in root_word:
+            same_words.append(i.title())
+
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
